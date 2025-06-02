01f8cc212ccc3cf92be4015e795eb552e8775744
Abhay Ghante <ghanteabhay@gmail.com>
2025-06-02 20:48:42 +0530

    added docker login

--- scripts/start.container.sh
+++ scripts/start.container.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+#login 
+echo "Abhay@2001" | docker login docker.io --username abhayghante --password-stdin
+
 #pull
 docker pull abhayghante/flask-app:latest
+
 #run 
 docker run -d -p 5000:5000 abhayghante/flask-app:latest
